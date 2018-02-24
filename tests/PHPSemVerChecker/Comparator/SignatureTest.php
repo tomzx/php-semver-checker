26109abb1f7d01d93fa7da1fd000a08892e90e89
Tom Rochette <roctom@gmail.com>
2018-02-24 17:25:45 -0500

    Revert SignatureTest.

--- tests/PHPSemVerChecker/Comparator/SignatureTest.php
+++ tests/PHPSemVerChecker/Comparator/SignatureTest.php
@@ -2,187 +2,75 @@
 
 namespace PHPSemVerChecker\Test\Comparator;
 
-use PhpParser\Node\Expr\ConstFetch;
-use PhpParser\Node\Name;
 use PhpParser\Node\Param;
-use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\Stmt\ClassMethod;
 use PHPSemVerChecker\Comparator\Signature;
-use PHPSemVerChecker\Test\TestCase;
 
-class SignatureTest extends TestCase
+class SignatureTest extends \PHPUnit_Framework_TestCase
 {
-	public function testAnalyzeNoParameter()
+	public function testIdenticalSignaturesNoParameters()
 	{
-		$parametersA = [];
-		$parametersB = [];
-		$result = Signature::analyze($parametersA, $parametersB);
-		$expected = Signature::changeArray();
-		$this->assertSame($expected, $result);
+		$result = Signature::analyze([], []);
+
+		$expectedResult = [
+			'parameter_added' => false,
+			'parameter_removed' => false,
+			'parameter_renamed' => false,
+			'parameter_typing_added' => false,
+			'parameter_typing_removed' => false,
+			'parameter_default_added' => false,
+			'parameter_default_removed' => false,
+			'parameter_default_value_changed' => false,
+		];
+
+		$this->assertEquals($expectedResult, $result);
 	}
 
-	public function testAnalyzeParameterAdded()
+	public function testRenamedParameters()
 	{
-		$parametersA = [
-			new Param('a', null, 'A'),
+		$params1 = [new Param('testParameter')];
+		$params2 = [new Param('testRenamedParameter')];
+
+		$result = Signature::analyze($params1, $params2);
+
+		$expectedResult = [
+			'parameter_added' => false,
+			'parameter_removed' => false,
+			'parameter_renamed' => true,
+			'parameter_typing_added' => false,
+			'parameter_typing_removed' => false,
+			'parameter_default_added' => false,
+			'parameter_default_removed' => false,
+			'parameter_default_value_changed' => false,
 		];
-		$parametersB = [
-			new Param('a', null, 'A'),
-			new Param('b', new String_('someDefaultValue'), 'B'),
-		];
-		$result = Signature::analyze($parametersA, $parametersB);
-		$expected = Signature::changeArray();
-		$expected['parameter_added'] = [
-			0 => 1,
-		];
-		$this->assertSame($expected, $result);
+
+		$this->assertEquals($expectedResult, $result);
 	}
 
-	public function testAnalyzeParameterRemoved()
+	public function testAddedParameters()
 	{
-		$parametersA = [
-			new Param('a', null, 'A'),
-			new Param('b', new String_('someDefaultValue'), 'B'),
+		$params1 = [
+			new Param('testParameter'),
 		];
-		$parametersB = [
-			new Param('a', null, 'A'),
-		];
-		$result = Signature::analyze($parametersA, $parametersB);
-		$expected = Signature::changeArray();
-		$expected['parameter_removed'] = [
-			0 => 1,
-		];
-		$this->assertSame($expected, $result);
-	}
 
-	public function testAnalyzeParameterRenamed()
-	{
-		$parametersA = [
-			new Param('a', null, 'A'),
-			new Param('b', new String_('someDefaultValue'), 'B'),
+		$params2 = [
+			new Param('testParameter'),
+			new Param('testAddedParameter'),
 		];
-		$parametersB = [
-			new Param('c', null, 'A'),
-			new Param('d', new String_('someDefaultValue'), 'B'),
-		];
-		$result = Signature::analyze($parametersA, $parametersB);
-		$expected = Signature::changeArray();
-		$expected['parameter_renamed'] = [
-			0 => ['before' => 'a', 'after' => 'c'],
-			1 => ['before' => 'b', 'after' => 'd'],
-		];
-		$this->assertSame($expected, $result);
-	}
 
-	public function testAnalyzeParameterRenamedX()
-	{
-		$parametersA = [
-			new Param('a', null, 'A'),
-			new Param('b', new String_('someDefaultValue'), 'B'),
-		];
-		$parametersB = [
-			new Param('b', new String_('someDefaultValue'), 'B'),
-			new Param('a', null, 'A'),
-		];
-		$result = Signature::analyze($parametersA, $parametersB);
-		$expected = Signature::changeArray();
-		//$expected['parameter_renamed'] = [
-		//	0 => ['before' => 'a', 'after' => 'c'],
-		//	1 => ['before' => 'b', 'after' => 'd'],
-		//];
-		$this->assertSame($expected, $result);
-	}
+		$result = Signature::analyze($params1, $params2);
 
-	public function testAnalyzeParameterTypingAdded()
-	{
-		$parametersA = [
-			new Param('a', null),
+		$expectedResult = [
+			'parameter_added' => true,
+			'parameter_removed' => false,
+			'parameter_renamed' => false,
+			'parameter_typing_added' => false,
+			'parameter_typing_removed' => false,
+			'parameter_default_added' => false,
+			'parameter_default_removed' => false,
+			'parameter_default_value_changed' => false,
 		];
-		$parametersB = [
-			new Param('a', null, 'A'),
-		];
-		$result = Signature::analyze($parametersA, $parametersB);
-		$expected = Signature::changeArray();
-		$expected['parameter_typing_added'] = [
-			0 => ['A'],
-		];
-		$this->assertSame($expected, $result);
-	}
 
-	public function testAnalyzeParameterTypingRemoved()
-	{
-		$parametersA = [
-			new Param('a', null, 'A'),
-		];
-		$parametersB = [
-			new Param('a', null),
-		];
-		$result = Signature::analyze($parametersA, $parametersB);
-		$expected = Signature::changeArray();
-		$expected['parameter_typing_removed'] = [
-			0 => ['A'],
-		];
-		$this->assertSame($expected, $result);
-	}
-
-	public function testAnalyzeParameterDefaultAdded()
-	{
-		$parametersA = [
-			new Param('a'),
-		];
-		$parametersB = [
-			new Param('a', new ConstFetch(new Name('null'))),
-		];
-		$result = Signature::analyze($parametersA, $parametersB);
-		$expected = Signature::changeArray();
-		$expected['parameter_default_added'] = [
-			0 => [new ConstFetch(new Name('null'))],
-		];
-		$this->assertEquals($expected, $result);
-	}
-
-	public function testAnalyzeParameterDefaultRemoved()
-	{
-		$parametersA = [
-			new Param('a', new ConstFetch(new Name('null'))),
-		];
-		$parametersB = [
-			new Param('a'),
-		];
-		$result = Signature::analyze($parametersA, $parametersB);
-		$expected = Signature::changeArray();
-		$expected['parameter_default_removed'] = [
-			0 => [new ConstFetch(new Name('null'))],
-		];
-		$this->assertEquals($expected, $result);
-	}
-
-	public function testAnalyzeParameterDefaultValueChanged()
-	{
-		$parametersA = [
-			new Param('a', new ConstFetch(new Name('null'))),
-		];
-		$parametersB = [
-			new Param('a', new ConstFetch(new Name('true'))),
-		];
-		$result = Signature::analyze($parametersA, $parametersB);
-		$expected = Signature::changeArray();
-		$expected['parameter_default_value_changed'] = [
-			0 => [new ConstFetch(new Name('null'))],
-		];
-		$this->assertEquals($expected, $result);
-	}
-
-	public function testAnalyzeParameterAddedWithDefault()
-	{
-		$parametersA = [];
-		$parametersB = [
-			new Param('b', new String_('someDefaultValue'), 'B'),
-		];
-		$result = Signature::analyze($parametersA, $parametersB);
-		$expected = Signature::changeArray();
-		//$expected['parameter_default_value_changed'] = [
-		//	0 => [new ConstFetch(new Name('null'))],
-		//];
-		$this->assertEquals($expected, $result);
+		$this->assertEquals($expectedResult, $result);
 	}
 }
